f81a3afa8a2dba435e7be60604ac676e12b33d48
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-08-14 13:59:10 -0600

    modify config for oauth and server setup

--- truly-badge/config/oauth.php
+++ truly-badge/config/oauth.php
@@ -1,8 +1,8 @@
 <?php
 return[
-    'clientID' => "beECXaQzYZOvr5DgrSw3ntX4lfZOfoJwDtFMX2N0UOc",
-    'clientSecret'=>"Y9Mo9s4fzRxo23dvzFO8h1v5FX5pp3xYKAqGicDuG70",
-    'redirectURL'=>"https://1197-43-230-206-233.ngrok.io/oauth_sdk/redirect_uri/",
+    'clientID' => env('CLIENT_ID'),
+    'clientSecret' => env('CLIENT_SECRET'),
+    'redirectURL'=> env('REDIRECT_URL'),
     'Scopes' => [
         "read_product", "write_product","read_shop","write_shop"
     ],
