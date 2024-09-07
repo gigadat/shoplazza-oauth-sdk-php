12587eeeffb96312b6656d239c7e14b29a445d5a
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-06 21:53:31 -0600

    get siteId from cookie if not found in url

--- truly-badge/public/js/trulybadge_qa.js
+++ truly-badge/public/js/trulybadge_qa.js
@@ -3,8 +3,20 @@
 
     var siteId = new URLSearchParams(window.location.search).get('siteId');
 
+    // get siteId from cookie
+    if (!siteId) {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].split('=');
+            if (cookie[0].trim() === 'siteId') {
+                siteId = cookie[1];
+                break;
+            }
+        }
+    }
+
     console.log('siteId found: ', siteId);
-    
+
     script.src = 'https://kmgu6d1qxk.execute-api.us-east-2.amazonaws.com/test/api/tlv1?siteId=' + siteId;
     script.async = true;
 
