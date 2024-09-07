67899caccb054a2241d4820a731f47545f303458
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-07 00:46:46 -0600

    if tlv1 script already exists, replace it

--- truly-badge/public/js/trulybadge_qa.js
+++ truly-badge/public/js/trulybadge_qa.js
@@ -1,10 +1,15 @@
 (function () {
-    var script = document.createElement('script');
-
     const scripts = Array.from(document.getElementsByTagName('script'));
     const trulyBadgeScript = scripts.find(s => s.src.includes('trulybadge_qa.js'));
     const siteId = trulyBadgeScript ? new URLSearchParams(new URL(trulyBadgeScript.src).search).get('siteId') : null;
+
+    // if script with tlv1 already exists, replace it with the new one
+    const existingScript = scripts.find(s => s.src.includes('tlv1'));
+    if (existingScript) {
+        existingScript.remove();
+    }
     
+    var script = document.createElement('script');
     script.src = 'https://kmgu6d1qxk.execute-api.us-east-2.amazonaws.com/test/api/tlv1?siteId=' + siteId;
     script.async = true;
 
