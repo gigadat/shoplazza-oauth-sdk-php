8b400b39d317da1e2cf584c4b174bb4c4e507851
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-06 21:28:16 -0600

    update prod script

--- truly-badge/public/js/trulybadge.js
+++ truly-badge/public/js/trulybadge.js
@@ -1,7 +1,9 @@
 (function () {
     var script = document.createElement('script');
 
-    script.src = 'https://badge.trulylegit.com/api/tlv1?siteId=';
+    var siteId = new URLSearchParams(window.location.search).get('siteId');
+
+    script.src = 'https://badge.trulylegit.com/api/tlv1?siteId=' + siteId;
     script.async = true;
 
     var entry = document.getElementsByTagName('script')[0];
