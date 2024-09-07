d978c6bac6ef24c1cff868eb2fc5824c59eafdfc
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-06 22:42:26 -0600

    use diff way to get siteid

--- truly-badge/public/js/trulybadge_qa.js
+++ truly-badge/public/js/trulybadge_qa.js
@@ -1,14 +1,17 @@
 (function () {
     var script = document.createElement('script');
 
-    var scripts = document.getElementsByTagName('script');
-    let siteId = null;
-    for (let i = 0; i < scripts.length; i++) {
-        if (scripts[i].src.includes('trulybadge_qa.js')) {
-            siteId = new URLSearchParams(scripts[i].src.split('?')[1]).get('siteId');
-            break;
-        }
-    }
+    // var scripts = document.getElementsByTagName('script');
+    // let siteId = null;
+    // for (let i = 0; i < scripts.length; i++) {
+    //     if (scripts[i].src.includes('trulybadge_qa.js')) {
+    //         siteId = new URLSearchParams(scripts[i].src.split('?')[1]).get('siteId');
+    //         break;
+    //     }
+    // }
+    const scripts = Array.from(document.getElementsByTagName('script'));
+    const trulyBadgeScript = scripts.find(s => s.src.includes('trulybadge_qa.js'));
+    const siteId = trulyBadgeScript ? new URLSearchParams(new URL(trulyBadgeScript.src).search).get('siteId') : null;
 
     console.log('siteId found: ', siteId);
     
