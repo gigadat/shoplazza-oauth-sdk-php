b51731891468d1f91a6f99e2165e13dae8b5a9d7
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-06 20:28:31 -0600

    hard code truly qa site id

--- truly-badge/public/js/trulybadge_qa.js
+++ truly-badge/public/js/trulybadge_qa.js
@@ -1,7 +1,7 @@
 (function () {
     var script = document.createElement('script');
 
-    script.src = 'https://kmgu6d1qxk.execute-api.us-east-2.amazonaws.com/test/api/tlv1?siteId=';
+    script.src = 'https://kmgu6d1qxk.execute-api.us-east-2.amazonaws.com/test/api/tlv1?siteId=35d98472-1783-4237-a7a3-7dcb84cf672a';
     script.async = true;
 
     var entry = document.getElementsByTagName('script')[0];
