05186449fde538c09c12793d4441e6b288d70ac6
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-11 13:00:17 -0600

    test deploy

--- truly-badge/resources/js/Pages/TrulyBadge/Badges/Index.vue
+++ truly-badge/resources/js/Pages/TrulyBadge/Badges/Index.vue
@@ -57,7 +57,7 @@
                                 Save
                             </button>
 
-                            {{ form.siteId }}
+                            Form Site Id: {{ form.siteId }}
                         </div>
                     </div>
                     <div class="col-span-1 bg-gray-900 text-white w-full rounded-2xl px-4 pt-4 pb-10">
