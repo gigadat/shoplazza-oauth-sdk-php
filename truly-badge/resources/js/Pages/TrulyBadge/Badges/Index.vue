a4381dfc4072712dd3c0f2cbe8f6563e0135053d
Philip Okanlawon <philip.okanlawon16@gmail.com>
2024-09-11 20:44:50 -0600

    finish badges page

--- truly-badge/resources/js/Pages/TrulyBadge/Badges/Index.vue
+++ truly-badge/resources/js/Pages/TrulyBadge/Badges/Index.vue
@@ -11,15 +11,15 @@
                         Badge Installation Tool
                     </h1>
                 </div>
-                <div class="w-1/2 pt-4 flex text-center">
-                    Dolor sit amet consectetur. Semper enim suspendisse eget egestas eu diam elementum sit, sed ultrices
+                <div class="w-3/5 pt-4 flex text-center font-articulat">
+                    Use our automatic badge installation tool, to display the badges on your store's website.
                 </div>
                 <div class="pt-8 w-full justify-between grid grid-cols-3 gap-4">
-                    <div class="col-span-2 bg-white text-gray-900 w-full rounded-2xl px-16 pt-4 pb-10">
-                        <h3 class="text-lg font-semibold text-gray-900 text-center pb-4">
+                    <div class="col-span-2 bg-white text-gray-900 w-full rounded-2xl 2xl:py-10">
+                        <h3 class="text-lg font-bold text-gray-900 text-center px-16 py-4">
                             Paste Your Site ID
                         </h3>
-                        <div>
+                        <div class="px-16 pb-6">
                             <label
                                 for="siteId"
                                 class="font-semibold text-sm text-gray-700"
@@ -30,7 +30,7 @@
                                 id="siteId"
                                 v-model="form.siteId"
                                 class="shadow-sm block w-full sm:text-sm rounded-md"
-                                :class="`border-${borderColor}-500 focus:ring-${borderColor}-600 focus:border-${borderColor}-600`"
+                                :class="borderStyle"
                                 required="true"
                                 @input="clearError('siteId')"
                             >
@@ -57,9 +57,40 @@
                                 Save
                             </button>
                         </div>
+                        <div class="border-t border-gray-300 text-center">
+                            <h3 class="text-lg font-bold text-gray-900 px-16 pt-4">
+                                Place Static Badges Option
+                            </h3>
+                            <div class="mt-2 text-xs text-gray-500 font-articulat px-16">
+                                Set an id attribute on the div element you created 
+                                and set the id value based on the type of badge to display.
+                            </div>
+                            <div class="text-sm font-semibold text-gray-900 px-16 py-10 flex flex-col gap-2">
+                                <div>id="truly-certified-badge" - for the Verified Business badge</div>
+                                <div>id="truly-secured-badge" - for the Secure Site badge</div>
+                                <div>id="truly-shield-badge" - for the Truly Shield badge</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-span-1 bg-gray-900 text-white w-full rounded-2xl px-4 pt-4 pb-10">
-                        right
+                    <div class="col-span-1 bg-gray-900 text-white w-full rounded-2xl px-4 2xl:px-6 pt-4 pb-10 2xl:py-6">
+                        <div class="text-2xl 2xl:text-4xl font-extrabold">
+                            Badge Customization.
+                        </div>
+                        <div class="font-articulat pt-2">
+                            Login to your Truly Legit account to customize your badges further.
+                        </div>
+                        <div class="py-14 2xl:py-20 flex flex-col items-left justify-center gap-6 px-8">
+                            <VerifiedBusiness class="w-full"/>
+                            <SecureSite class="w-5/6"/>
+                        </div>
+                        <div class="flex justify-center">
+                            <button
+                                @click="goToCustomizeBadges"
+                                class="outline-none px-6 py-2 rounded shadow-sm text-white text-sm bg-blue-600 hover:bg-blue-700 cursor-pointer"
+                            >
+                                Customize
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -73,17 +104,19 @@ import { computed, ref } from 'vue'
 import { has, isEmpty } from 'lodash';
 import { Head, useForm } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import SecureSite from '@/Components/Badges/SecureSite.vue';
+import VerifiedBusiness from '@/Components/Badges/VerifiedBusiness.vue';
 
 const form = useForm({
     siteId: '',
 });
 
-const hasError = (propName) => {
-    return has(form.errors, propName);
-};
-
-const borderColor = computed(() => {
-    return hasError('siteId') ? 'red' : 'blue';
+const borderStyle = computed(() => {
+    if (getErrorMessage('siteId') === null) {
+        return 'border-blue-500 focus:ring-blue-600 focus:border-blue-600';
+    } else {
+        return 'border-red-500 focus:ring-red-600 focus:border-red-600';
+    }
 });
 
 let formSubmitted = ref(false);
@@ -97,6 +130,14 @@ function submit() {
                 form.siteId = '';
             },
         });
+    }
+}
+
+function goToCustomizeBadges() {
+    if (import.meta.env.VUE_APP_ENV === 'production') {
+        window.open('https://portal.trulylegit.com/badges', '_blank');
+    } else {
+        window.open('https://qa-portal.trulylegit.com/badges', '_blank');
     }
 }
 
